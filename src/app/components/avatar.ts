e500a2864cff64a3c8658184b95bcdc0be786788
arunwithasmile <arunwithasmile@gmail.com>
2025-09-12 13:30:30 +0530

    Showing only first and last initials

--- src/app/components/avatar.ts
+++ src/app/components/avatar.ts
@@ -14,10 +14,14 @@ export class Avatar {
     protected readonly bgColos = ['#76a2a2', '#b76a43ff', '#698fbc', '#a47665', '#73865d', '#8472a3'];
 
     get initials(): string {
-        return this.name.split(' ').map(n => n[0]).join('').toUpperCase();
+        const tokens = this.name.split(' ');
+        if (tokens.length == 1) {
+            return tokens[0][0].toUpperCase();
+        }
+        return (tokens[0][0] + tokens[tokens.length - 1][0]).toUpperCase();
     }
 
-    get randomColor(): string {
+    get hashColor(): string {
         let hash = 0;
         for (let i = 0; i < this.name.length; i++) {
             hash = this.name.charCodeAt(i) + ((hash << 5) - hash);
